b1179504d5833da4c871b8af8b7dc21263da0c61
nickShermeister <ncsherman159@gmail.com>
2017-11-11 19:04:24 -0500

    Pushing some changes in case something happens to comp.

--- runner.py
+++ runner.py
@@ -12,24 +12,67 @@ class ChessGame():
         self.running = True
         self.turn = True    #True means player makes first move; else AI makes first move.
         self.first = self.turn
-
+        self.whiteGraveyard = {'':[], 'R':[], 'N':[], 'B':[], 'K':[], 'Q':[]} #empty string = pawn
+        self.blackGraveyard = {'':[], 'r':[], 'n':[], 'b':[], 'k':[], 'q':[]}
+        self.whiteLocations = {'':[], 'R':[], 'N':[], 'B':[], 'K':[], 'Q':[]}
+        self.blackLocations = {'':[], 'r':[], 'n':[], 'b':[], 'k':[], 'q':[]}
+        self.resetBoard()
 
         self.gameLoop()
 
     def movePiece(self, command):
-        stripped_command = ''.join(l for l in command if not l.isupper())
         try:
             hi = self.board.push_san(command)
+            # self.convertToLocation(stripped_command)
         except:
             print(stripped_command)
             hi = "Invalid command."
         print(hi)
-        self.convertToLocation(stripped_command)
+
+        if len(hi.uci()) == 4:
+            self.convertToLocation(hi.uci())
+
+    def findLocPiece(self, location):
+        """
+        Takes in a 2 letter/number string thatgives a square (e.g. a1, h8, etc.)
+        returns P for white pawn, p for black pawn.
+        """
+        for x in self.whiteLocations:
+            if location in self.whiteLocations[x]:
+                if x == "":
+                    return "P"
+                else:
+                    return x
+        for x in self.blackLocations:
+            if location in self.blackLocations[x]:
+                if x == "":
+                    return "p"
+                else:
+                    return x
+        return -1
+
 
     def printBoard(self):
         print(self.board)
 
     def resetBoard(self):
+        #TODO: ROUTE TO MOVE PIECES BACK
+
+
+        self.whiteLocations[''] = ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2']
+        self.whiteLocations['R'] = ['a1', 'h1']
+        self.whiteLocations['N'] = ['b1', 'g1']
+        self.whiteLocations['B'] = ['c1', 'f1']
+        self.whiteLocations['K'] = ['d1']
+        self.whiteLocations['Q'] = ['e1']
+
+        self.blackLocations[''] = ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7']
+        self.blackLocations['r'] = ['a8', 'h8']
+        self.blackLocations['n'] = ['b8', 'g8']
+        self.blackLocations['b'] = ['c8', 'f8']
+        self.blackLocations['k'] = ['d8']
+        self.blackLocations['q'] = ['e8']
+
         self.board.reset()
         self.printBoard()
 
