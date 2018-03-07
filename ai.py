e940efb628925cd9e6b9eea01a20183130526a0a
Eric Miller <eric@legoaces.org>
2018-03-06 20:27:26 -0500

    Added template in AI.py

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@ The ai behind our Wiznerds Chess.
 """
 import chess
 import chess.uci
+from chess import Board, Move
 import random
 from motionPlanner import *
 from collections import defaultdict
@@ -13,3 +14,12 @@ class aiController:
         self.engine = chess.uci.popen_engine('libs/stockfish-8-linux/Linux/stockfish_8_x64')
         self.engine.uci()
         
+
+    def getMove(self, boardState, color="w"):
+        """
+
+        :param Board boardState: The state of the board
+        :param str color: Whose move it is
+        :return Move: What the AI wants to do
+        """
+        return Move.from_uci('a1a1')
